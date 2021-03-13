958c926c17f349f500f84651f0c4944cdf15c9f8
GEROM <ty.gerom@gmail.com>
2021-03-13 08:38:28 +0300

    created classes for projects and tasks/ созданны классы для проектов и задач

---
+++ ToDoListWpf/ProjectTask.cs
@@ -0,0 +1,38 @@
+﻿namespace ToDoListWpf
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    
+    public class ProjectTask
+    {
+        ProjectTask()
+        {
+
+        }
+
+        ProjectTask(int indexNumber, string description, bool performed = false)
+        {
+            IndexNumber = indexNumber;
+            Description = description;
+            Performed = performed;
+        }
+
+        /// <summary>
+        /// порядковый номер, важность
+        /// </summary>
+        public int IndexNumber { get; set; }
+
+        /// <summary>
+        /// описание задачи
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// флаг выполнения
+        /// </summary>
+        public bool Performed { get; set; }
+    }
+}
---
+++ ToDoListWpf/Projects.cs
@@ -0,0 +1,21 @@
+﻿namespace ToDoListWpf
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Projects
+    {
+        /// <summary>
+        /// Имя проекта
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Список задач
+        /// </summary>
+        public List<ProjectTask> Tasks = new();
+    }
+}
