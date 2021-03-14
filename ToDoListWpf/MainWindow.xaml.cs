bf419f54e789f7e58c85569f7ac9afb259f9aca6
GEROM <ty.gerom@gmail.com>
2021-03-14 14:00:48 +0300

    implemented the ability to create and delete projects / реализована возможность создавать и удалять проекты

--- ToDoListWpf/MainWindow.xaml.cs
+++ ToDoListWpf/MainWindow.xaml.cs
@@ -23,6 +23,47 @@ namespace ToDoListWpf
         public MainWindow()
         {
             InitializeComponent();
+            //Loaded += MainWindow_Loaded;
+        }
+
+
+        //private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        //{
+        //    var selected = listProjects.SelectedItem;
+        //    var project = (Project)selected;
+        //    if (project != null)
+        //    {
+        //        foreach (var task in project.Tasks)
+        //        {
+        //            tasksView.Items.Add(task);
+        //        }
+        //    }
+        //}
+
+        Projects projects = new();
+
+        private void AddProjects_Click(object sender, RoutedEventArgs e)
+        {
+            WindowAddNewProject windowAddNewProject = new();
+            if (windowAddNewProject.ShowDialog() == true)
+            {
+                var nameInTextBox = windowAddNewProject.NameInTextBox;
+                var i = projects.AddProject(nameInTextBox);
+                listProjects.Items.Add(projects.ListProjects[i]);
+            }
+        }
+
+        private void DelProjects_Click(object sender, RoutedEventArgs e)
+        {
+            var selected = listProjects.SelectedItem;
+
+            if (selected is null)
+            {
+                return;
+            }
+
+            listProjects.Items.Remove(selected);
+            projects.DeleteProject((Project)selected);
         }
     }
 }
